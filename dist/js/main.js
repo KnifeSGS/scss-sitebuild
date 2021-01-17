da532f31c3bdc19029e22b01f1b61093ed8913c1
KnifeSGS <knife.sgs@gmail.com>
2021-01-17 23:30:47 +0100

    alapok navbar, section1, section2, section 3 folyamatban

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,31 +1,8 @@
+/* eslint-disable indent */
 $(window).scroll(function () {
-    $('.navbar').toggleClass('scrolled', $(this).scrollTop() > 50);
-    $('.menuBurger').toggleClass('scrolled', $(this).scrollTop() > 50);
+    $('header').toggleClass('scrolled', $(this).scrollTop() > 50);
+    $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
+    $('.nav__container').toggleClass('scrolled', $(this).scrollTop() > 50);
     $('.brandName').toggleClass('scrolled', $(this).scrollTop() > 50);
+    $('.menuBurger').toggleClass('scrolled', $(this).scrollTop() > 50);
 });
-
-function scrollTo(className, rate = 15) {
-    const elements = document.getElementsByClassName(className).length; if (elements === 1) {
-        // eslint-disable-next-line max-len
-        const objective = document.getElementsByClassName(className)[0]; const page = document.documentElement; const start = page.scrollTop; const end = objective.offsetTop; const difference = end - start; const sign = Math.sign(difference); const moveScroll = (position) => {
-            // eslint-disable-next-line max-len
-            const gap = position - start; if (gap < (Math.abs(difference) - rate)) { position += rate; page.scrollTop = start + (gap * sign); return setTimeout(() => { moveScroll(position); }, 1); }
-            page.scrollTop = end; return end;
-        }; if (start !== end) moveScroll(start);
-    } else console.log(`Warning on scrollTo(): Multiple class name "${className}" were found`);
-}
-
-window.onclick = (e) => {
-    e.preventDefault();
-
-    switch (e.target.tagName) {
-        case 'A':
-            scrollTo(e.target.href.split('#')[1]);
-            break;
-
-        case 'SPAN':
-            scrollTo(e.target.parentElement.href.split('#')[1]);
-
-        default: break;
-    }
-};
